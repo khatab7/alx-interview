10f6e8ac68099b657883bce2fa150ffbdae7fb2d
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 <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>
2025-02-13 00:06:29 +0200

    two dimension matrix 90 degrees clockwise

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -3,7 +3,6 @@
 rotate 2D matrix
 """
 
-
 def rotate_2d_matrix(matrix):
     """
     two dimension matrix 90 degrees clockwise
@@ -13,11 +12,8 @@ def rotate_2d_matrix(matrix):
         y = (ln - i - 1)
         for j in range(i, y):
             x = (ln - 1 - j)
-
             tmp = matrix[i][j]
             matrix[i][j] = matrix[x][i]
-
             matrix[x][i] = matrix[y][x]
-
             matrix[y][x] = matrix[j][y]
             matrix[j][y] = tmp
